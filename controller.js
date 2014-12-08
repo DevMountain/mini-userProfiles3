2115974d85225736d7bd6254d24d200ff678b8fc
eanplatter <eanplatter@gmail.com>
2014-12-07 23:23:57 -0700

    first commit

--- controller.js
+++ controller.js
@@ -2,11 +2,9 @@ var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
   $scope.getUsers = function() {
-    mainService.getUsers().then(function(data) {
+    $scope.users= mainService.getUsers().then(function(data) {
       $scope.users = data.data.data;
-      console.log($scope.users)
     });
-    console.log($scope.users);
   }
 
   $scope.getUsers();
--- service.js
+++ service.js
@@ -1,12 +1,12 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
 
 
   this.getUsers = function() {
     return $http({
         method: 'GET',
-        url: 'http://reqr.es/api/users?page=3'
+        url: 'http://reqr.es/api/users?page=1'
     })
   }
 });
