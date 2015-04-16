ac194e1f0c6f72b125ef39572fceffc45f81ebce
kenostik <kenostik@gmail.com>
2015-04-16 11:49:25 -0600

    Complete!

--- service.js
+++ service.js
@@ -1,12 +1,19 @@
 var app = angular.module('userProfiles');
 
 app.service('mainService', function($http, $q) {
-
+	var deferred = $q.defer();
 
   this.getUsers = function() {
-    return $http({
+    $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
+    }).then(function(response){
+    	response = response.data.data
+    	for (var i = 0; i < response.length; i++) {
+    		response[i].first_name = '(First Name Witheld)';
+    	};
+    	deferred.resolve(response)
     })
+    return deferred.promise;
   }
 });
