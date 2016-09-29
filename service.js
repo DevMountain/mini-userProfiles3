64b7c77534bc951e4533f5b832e179ad6d650f0f
Jacob <admin@Mjolnir.local>
2016-09-29 13:32:01 -0600

    update

--- service.js
+++ service.js
@@ -1,10 +1,17 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var deferred = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
+    }).then(function(response) {
+      var parsedResponse = response.data.data
+      for(var i = 0; i < parsedResponse.length; i++) {
+        parsedResponse[i].first_name = 'Ralf'
+      }
+      deferred.resolve(parsedResponse)
     })
+    return deferred.promise;
   }
-
 });
